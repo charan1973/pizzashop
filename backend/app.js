1cc1db80d119fb1aa757bb1bc1e7c11b32b34202
charanvasu <v.vicky1973@gmail.com>
2020-12-27 20:58:33 +0530

    chore: only serve static files when in production

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const path = require("path")
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
-app.use(cors())
+app.use(cookieParser());
+app.use(cors());
 
 mongoose.connect(
   process.env.DB_URL,
@@ -21,35 +21,36 @@ mongoose.connect(
   () => console.log("DB connected")
 );
 
-
-
-
 // Core routes
 const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user")
-const coreRoutes = require("./routes/core")
+const userRoutes = require("./routes/user");
+const coreRoutes = require("./routes/core");
 
 // Admin routes
 const adminCategoryRoutes = require("./routes/admin/category");
 const adminItemRoutes = require("./routes/admin/item");
 const adminAddOnRoutes = require("./routes/admin/addOn");
-const adminOrderRoutes = require("./routes/admin/orderAdmin")
+const adminOrderRoutes = require("./routes/admin/orderAdmin");
 
 // Core api calls
 app.use("/api", authRoutes);
-app.use("/api", userRoutes)
-app.use("/api", coreRoutes)
+app.use("/api", userRoutes);
+app.use("/api", coreRoutes);
 
 // Admin api calls
 app.use("/api/admin", adminCategoryRoutes);
 app.use("/api/admin", adminItemRoutes);
 app.use("/api/admin", adminAddOnRoutes);
-app.use("/api/admin", adminOrderRoutes)
+app.use("/api/admin", adminOrderRoutes);
 
-app.use(express.static("../client/build"))
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("../client/build/index.html"))
-})
+if (process.env.NODE_ENV === 'production') {
+  // Serve frontend files
+  app.use(express.static("../client/build"));
+  // Getting all the routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("../client/build/index.html"));
+  });
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, (req, res) => {
