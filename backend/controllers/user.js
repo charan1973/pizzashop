40773ae42c5d35ee7ce811ae74a89656297ec173
charanvasu <v.vicky1973@gmail.com>
2020-12-30 21:18:54 +0530

    minor: fixed numbers returning wrong values when adding

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,11 +3,20 @@ const User = require("../models/User");
 const Item = require("../models/Item");
 
 exports.getUserProfile = async (req, res) => {
-  try{
-    const {address, email, firstName, lastName, fullName, _id} = await User.findById(req.user.id)
-    return res.json({user: {address, email, firstName, lastName, fullName, _id}})
-  }catch(err){
-    return res.json({error: "Error getting user"})
+  try {
+    const {
+      address,
+      email,
+      firstName,
+      lastName,
+      fullName,
+      _id,
+    } = await User.findById(req.user.id);
+    return res.json({
+      user: { address, email, firstName, lastName, fullName, _id },
+    });
+  } catch (err) {
+    return res.json({ error: "Error getting user" });
   }
 };
 
@@ -77,32 +86,47 @@ exports.createOrder = async (req, res) => {
   const { orderContent, addressId } = req.body;
 
   // Calculate amount
-  const bill = orderContent.reduce((a, content) => {
-    return (
+  const bill = orderContent.reduce(
+    (a, item) =>
       a +
-      (content.itemPrice + content.addOn.reduce((b, addOn) => b + addOn.addOnPrice, 0))) * content.quantity;
-  }, 0);
+      (item.itemPrice +
+        item.addOn.reduce((b, addOn) => b + addOn.addOnPrice, 0)) *
+        item.quantity,
+    0
+  );
 
   // Find address for adding to the order document
   const findUser = await User.findById(req.user.id);
-  const address = findUser.address.id(addressId)
+  const address = findUser.address.id(addressId);
 
   const newOrder = new Order({
     orderPrice: bill,
     address,
     orderContent,
-    customer: req.user.id
-  })
+    customer: req.user.id,
+  });
 
-  try{
-    const savedOrder = await newOrder.save()
-    const getOrder = await Order.findById(savedOrder._id).populate("orderContent.itemId orderContent.addOn._id")
+  try {
+    const savedOrder = await newOrder.save();
+    const getOrder = await Order.findById(savedOrder._id).populate(
+      "orderContent.itemId orderContent.addOn._id"
+    );
     // Check if the amount matches with the amount calculated after the database population
-    const checkBill = getOrder.orderContent.reduce((a, item) => 
-    a + (item.itemId.size[item.itemSize] + item.addOn.reduce((b, addon) => b + addon._id.addOnPrice, 0)) * item.quantity
-    ,0)
-    return res.json({message: checkBill === bill ? "Order placed successfully" : "Numbers doesn't add up"})
-  }catch(err){
-    return res.json({error: "Error placing the order"})
+    const checkBill = getOrder.orderContent.reduce(
+      (a, item) =>
+        a +
+        (item.itemId.size[item.itemSize] +
+          item.addOn.reduce((b, addon) => b + addon._id.addOnPrice, 0)) *
+          item.quantity,
+      0
+    );
+    return res.json({
+      message:
+        checkBill === bill
+          ? "Order placed successfully"
+          : "Numbers doesn't add up",
+    });
+  } catch (err) {
+    return res.json({ error: "Error placing the order" });
   }
 };
