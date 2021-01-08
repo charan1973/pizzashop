78883c428af5b8a6b56dc93c06b4c74fde2ab53f
charanvasu <v.vicky1973@gmail.com>
2021-01-08 17:18:00 +0530

    error handling page theme fixed

--- client/src/pages/error/Error.page.jsx
+++ client/src/pages/error/Error.page.jsx
@@ -1,16 +1,27 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
-import ErrorIcon from "../../assets/404.svg"
-
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import ErrorIcon from "../../assets/404.svg";
 
 const ErrorPage = () => {
-    return (
-        <Box d="flex" flexDirection="column" justifyContent="center" h="70vh">
-            <Image src={ErrorIcon} w="100%" />
-            <Text as="h2" textAlign="center" fontSize="20px">404 Error! Page not found.</Text>
-            <Button as={Link} to="/">Go Home</Button>
-        </Box>
-    )
-}
+  return (
+    <Box
+      d="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      h="80vh"
+    >
+      <Box w="40%">
+        <Image src={ErrorIcon} w="100%" mb="20px" />
+        <Text as="h2" textAlign="center" fontSize="20px">
+          404 Error! Page not found.
+        </Text>
+        <Button as={Link} to="/" w="100%">
+          Go Home
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
